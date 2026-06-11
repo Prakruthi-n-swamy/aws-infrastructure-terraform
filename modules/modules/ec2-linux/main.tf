4b2ef217afc2a58fbbb7958c977005bf796aeafb
Prakruthi-n-swamy <prakruthihello123@gmail.com>
2026-06-11 19:44:05 +0530

    Delete modules/modules directory

--- modules/modules/ec2-linux/main.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_instance" "linux_server" {
-
-  ami           = var.ami_id
-
-  instance_type = var.instance_type
-
-}
--- modules/modules/ec2-linux/outputs.tf
+++
@@ -1,5 +0,0 @@
-output "instance_id" {
-
-  value = aws_instance.linux_server.id
-
-}
